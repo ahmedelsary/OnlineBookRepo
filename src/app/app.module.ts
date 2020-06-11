3c9cc0e0b95f57d46c052e51722670fb5a1c7b35
Ahmed Sayed <ahmed.sayed.hassanien@gmail.com>
2020-06-11 11:13:31 +0200

    online book store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+// material
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+
+
+
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    //material 
+    MatToolbarModule,
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
